eab6ba37039db18401bf6aaf61982863fe4be0f4
JamesChenevert <shadowentertainmentproductions@gmail.com>
2025-05-14 11:04:28 -0400

    New Ver 0.0.1

--- MainGame.java
+++ MainGame.java
@@ -1,5 +1,5 @@
 public class MainGame {
     public static void main(String[] args) {
-
+        System.out.println("Hello... \nWelcome to \"Survive the Night: RE-ENLIGHTENED\" i have a question do you wish to change a man's life...? \nOh you can't type anything else other than \"Yes\" or \"No\"... \nOh well let's just get to the point... \nWould you like to start the game?");
     }
 }
